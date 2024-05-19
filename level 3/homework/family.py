0c1c53b88e85da790555986e88bc55162ab324a4
kote1jashiashvili <kotejashiashvili2013@gmail.com>
2024-05-19 20:24:19 +0400

    commit

---
+++ level 3/homework/family.py
@@ -0,0 +1,9 @@
+me = "kote jashiashvili"
+sister = "mariam jashiashvili"
+mom = "maiko kirvalidze"
+dad = "miro jashiashvili"
+
+
+
+
+print( "in my family, there are 4 people. me, my sister, my mom and my dad. ""i am " + me + " my sister is" + sister + " my mom is" + mom + " my dad is" + dad)
