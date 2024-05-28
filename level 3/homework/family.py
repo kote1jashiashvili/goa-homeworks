fa53d279a6f573bfa70ae7e4ca0a189ae50e133d
kote1jashiashvili <kotejashiashvili2013@gmail.com>
2024-05-28 16:46:55 +0400

    commit

--- level 3/homework/family.py
+++ level 3/homework/family.py
@@ -6,4 +6,4 @@ dad = "miro jashiashvili"
 
 
 
-print( "in my family, there are 4 people. me, my sister, my mom and my dad. ""i am " + me + " my sister is" + sister + " my mom is" + mom + " my dad is" + dad)
+print( "in my family, there are 4 people. me, my sister, my mom and my dad. ""i am " + me + " my sister is " + sister + " my mom is " + mom + " my dad is " + dad)
