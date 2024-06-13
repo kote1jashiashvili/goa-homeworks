26c59ba175396b421e7b6636537a74e2a99c1e9a
kote1jashiashvili <kotejashiashvili2013@gmail.com>
2024-06-13 20:14:22 +0400

    commit

---
+++ level 10/classwork/input.py
@@ -0,0 +1,8 @@
+
+input("enter your name:")
+input("enter your surname:")
+input("enter your age:")
+
+print("thanks for your support")
+
+
---
+++ level 10/classwork/python.py
@@ -0,0 +1,11 @@
+
+
+#about me
+my_name = "Kote"
+my_surname = "jashiashvili"
+my_age = 10
+
+#printing
+print("my name is " + my_name)
+print("my surname is " + my_surname)
+print("i am " + my_age + " years old")
